b16f469dd64d3842cca5264550b9dad8465126c0
JoasFyllipe <joassolano2908@gmail.com>
2025-03-10 22:12:22 -0300

    repository

--- src/main/java/io/github/JoasFyllipe/repository/OculosRepository.java
+++ src/main/java/io/github/JoasFyllipe/repository/OculosRepository.java
@@ -1,9 +1,18 @@
 package io.github.JoasFyllipe.repository;
 
+import io.github.JoasFyllipe.dto.OculosDTO;
+import io.github.JoasFyllipe.model.CorArmacao;
 import io.github.JoasFyllipe.model.Oculos;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.List;
+
+
 @ApplicationScoped
 public class OculosRepository implements PanacheRepository<Oculos> {
+
+    public List<Oculos> findByCor(CorArmacao corArmacao){
+        return list("corArmacao", corArmacao);
+    }
 }
